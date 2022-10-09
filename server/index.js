23c08ecb328ad47857fa65b67bfa2ce897989827
skycader <skycader@github.com>
2022-10-09 13:53:09 +0300

    Added cors 2

--- server/index.js
+++ server/index.js
@@ -2,20 +2,16 @@ const express = require("express")
 const app = express()
 const db = require("./db/products")
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
-
 app.get("/",(req,res)=>{
   res.status(200).json({api: "green-store",success: true})
 })
 
 app.post("/products", async (req,res)=>{
   const results = await db.getAllProducts()
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   res.status(200).json({data: results})
 })
 
